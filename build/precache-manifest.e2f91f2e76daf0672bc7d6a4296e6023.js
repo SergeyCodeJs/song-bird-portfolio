02b6f6a672fbf0b70a9c36e072cb392b93c58549
SergeyCodeJs <sergeysteh@gmail.com>
2020-10-15 19:06:29 +0400

    feat: add build

---
+++ build/precache-manifest.e2f91f2e76daf0672bc7d6a4296e6023.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d622d94fe42cba2da33f19356ec4aba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972220cff737f2355356",
+    "url": "/static/css/2.e3f36e05.chunk.css"
+  },
+  {
+    "revision": "e8587403aa0a015fa97b",
+    "url": "/static/css/main.9d4c9859.chunk.css"
+  },
+  {
+    "revision": "972220cff737f2355356",
+    "url": "/static/js/2.6dd3c145.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.6dd3c145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8587403aa0a015fa97b",
+    "url": "/static/js/main.182fc324.chunk.js"
+  },
+  {
+    "revision": "d804a2a1635ee460d7b7",
+    "url": "/static/js/runtime-main.3fd985ff.js"
+  },
+  {
+    "revision": "0cfc29e3e69f8d0a6083fed65510eba4",
+    "url": "/static/media/17023.0cfc29e3.ttf"
+  },
+  {
+    "revision": "eeca044ef8fa4525d3923045f1adbd6c",
+    "url": "/static/media/ZhiMangXing-Regular.eeca044e.ttf"
+  },
+  {
+    "revision": "f99242540aff1b660898479fc56d6b0f",
+    "url": "/static/media/ill_bird__footer.f9924254.png"
+  },
+  {
+    "revision": "2c44b59cb005d77b1e0c43584e039f3c",
+    "url": "/static/media/sf-ui-display-bold-58646a511e3d9.2c44b59c.woff"
+  },
+  {
+    "revision": "a06f68e84c3c41498789c7471809aeaa",
+    "url": "/static/media/sf-ui-display-heavy-586470160b9e5.a06f68e8.woff"
+  }
+]);
